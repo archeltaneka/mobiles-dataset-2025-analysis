2cd4c4b74d366fcf1433637b3b1998864ebfe74a
Archel Taneka <archeltaneka@gmail.com>
2025-12-06 14:13:42 +1100

    [Improvement] Added assert messages on data loader tests

--- tests/test_data_loader.py
+++ tests/test_data_loader.py
@@ -11,10 +11,10 @@ def test_load_data(sample_csv_path, sample_raw_dataframe):
     loader = MobilesData(data_dir=sample_csv_path.parent, fname=sample_csv_path.name)
     df = loader.load_data()
 
-    assert not df.empty
-    assert os.path.getsize(os.path.join(sample_csv_path.parent, sample_csv_path.name)) > 0
-    assert list(df.columns) == list(sample_raw_dataframe.columns)
-    assert_frame_equal(df, sample_raw_dataframe)
+    assert not df.empty, 'Loaded data is empty'
+    assert os.path.getsize(os.path.join(sample_csv_path.parent, sample_csv_path.name)) > 0, 'Loaded data is empty'
+    assert list(df.columns) == list(sample_raw_dataframe.columns), f'Loaded data must have the following columns: {sample_raw_dataframe.columns}'
+    assert_frame_equal(df, sample_raw_dataframe), 'Loaded data does not have the same values with the expected data'
 
 
 def test_load_data_file_not_found():
