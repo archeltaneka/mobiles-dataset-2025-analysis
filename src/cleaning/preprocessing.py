6f9293abce59bdfa33c045841ed2e49201d5c4b5
Archel Taneka <archeltaneka@gmail.com>
2025-12-06 14:13:03 +1100

    [Fix] Casted ram and internal memory back to float, resolved outlier clipping issue where all prices share the same bound values

--- src/cleaning/preprocessing.py
+++ src/cleaning/preprocessing.py
@@ -25,12 +25,12 @@ def fill_null_values(df):
 
 def cast_column_types(df):
     df['mobile_weight'] = df['mobile_weight'].astype('float')
-    df['ram'] = df['ram'].astype('int')
+    df['ram'] = df['ram'].astype('float')
     df['front_camera'] = df['front_camera'].astype('int')
     df['back_camera'] = df['back_camera'].astype('int')
     df['battery_capacity'] = df['battery_capacity'].astype('int')
     df['screen_size'] = df['screen_size'].astype('float')
-    df['internal_memory'] = df['internal_memory'].astype('int')
+    df['internal_memory'] = df['internal_memory'].astype('float')
 
     for col in df.columns:
         if col.startswith('launched_price'):
@@ -42,8 +42,8 @@ def cast_column_types(df):
 def clip_price_outliers(df):
     for col in df.columns:
         if col.startswith('launched_price'):
-            df['lower_bound'] = df[col].quantile(0.05)
-            df['upper_bound'] = df[col].quantile(0.95)
-            df[col] = df[col].clip(lower=df['lower_bound'], upper=df['upper_bound'])
+            df[f'{col}_lower_bound'] = df[col].quantile(0.05)
+            df[f'{col}_upper_bound'] = df[col].quantile(0.95)
+            df[col] = df[col].clip(lower=df[f'{col}_lower_bound'], upper=df[f'{col}_upper_bound'])
             
     return df
